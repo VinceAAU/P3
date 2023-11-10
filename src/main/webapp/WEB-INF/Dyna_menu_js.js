71343906cbaae54311534b749418ea920978a623
ZeroConfidence <113094244+ZeroConfidence@users.noreply.github.com>
2023-11-10 13:07:54 +0100

    work in progress uwu

--- src/main/webapp/WEB-INF/Dyna_menu_js.js
+++ src/main/webapp/WEB-INF/Dyna_menu_js.js
@@ -8,7 +8,6 @@ class Order_Item {
 
 const order = [];
 
-
 function addToOrder(itemName, selectedOptions, selectedAdditions) {
   const orderItem = new Order_Item(itemName);
   orderItem.selectedOptions = selectedOptions;
@@ -19,78 +18,91 @@ function addToOrder(itemName, selectedOptions, selectedAdditions) {
 }
 
 document.querySelector('div > button.add-to-order').addEventListener('click', (event) => {
-    const target = event.target;
+  const target = event.target;
 
- 
   if (target.classList.contains('add-to-order')) {
     const itemName = target.getAttribute('data-item-name');
-    const optionsgroup = document.getAttribute('.options');
-    const selecetedOptions = getSelectedOptions(optionsGroup);
+    const optionsGroup = document.querySelector('.option');
+    const selectedOptions = getSelectedOptions(optionsGroup);
     const additionGroup = document.querySelector('.addition');
     const selectedAdditions = getSelectedAdditions(additionGroup);
-    
-    addToOrder(itemName,selectedOptions,selectedAdditions);
-  }
-  function get Selectedoptions(optionsGroup) {
-    const buttons = optionGroup.querySelectorall('button');
-    const selectedOptions =[];
 
-    buttons.forEach((buttons) => {
-      if (buttins.classList.contains('selected')){
+    addToOrder(itemName, selectedOptions, selectedAdditions);
+  }
+
+  function getSelectedOptions(group) {
+    const buttons = group.querySelectorAll('button');
+    const selectedOptions = [];
+
+    buttons.forEach((button) => {
+      if (button.classList.contains('selected')) {
         const option = button.getAttribute('data-option');
         selectedOptions.push(option);
       }
-    }); 
+    });
 
     return selectedOptions;
   }
 
-  function getSelectedAdditions(additionGroup) {
-  const buttons = additionGroup.querySelectorAll('button');
-  const selectedAdditions = [];
+  function getSelectedAdditions(group) {
+    const buttons = group.querySelectorAll('button');
+    const selectedAdditions = [];
 
-  buttons.forEach((button) => { if (button.classList.contains('selected')) {
-      const addition = button.getAttribute('data-addition');
-      selectedAdditions.push(addition);
-  });
-  return selectedAdditions;
+    buttons.forEach((button) => {
+      if (button.classList.contains('selected')) {
+        const addition = button.getAttribute('data-addition');
+        selectedAdditions.push(addition);
+      }
+    });
+
+    return selectedAdditions;
   }
-    
-  
-  
 
-const optionsGroup = document.querySelector('.options'); 
-if (optionsGroup) {
-  const buttons = optionsGroup.querySelectorAll('button');
-  const minSelections = parseInt(optionsGroup.getAttribute('data-min-selections'));
-  const maxSelections = parseInt(optionsGroup.getAttribute('data-max-selections'));
+  const optionsGroup = document.querySelector('.option');
+  if (optionsGroup) {
+    const buttons = optionsGroup.querySelectorAll('button');
+    const minSelections = parseInt(optionsGroup.getAttribute('data-min-selections'));
+    const maxSelections = parseInt(optionsGroup.getAttribute('data-max-selections'));
 
-  const selectedOptions = new Set();
+    const selectedOptions = new Set();
 
-  buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-      const option = button.getAttribute('data-option');
+    buttons.forEach((button) => {
+      button.addEventListener('click', () => {
+        const option = button.getAttribute('data-option');
+        console.log('option clicked');
 
-      if (selectedOptions.has(option)) {
-        selectedOptions.delete(option);
-        button.classList.remove('selected');
-      } else {
-        if (selectedOptions.size < maxSelections) {
-          selectedOptions.add(option);
+        if (selectedOptions.has(option)) {
+          selectedOptions.delete(option);
+          button.classList.remove('selected');
+        } else {
+          if (selectedOptions.size < maxSelections) {
+            selectedOptions.add(option);
+            button.classList.add('selected');
+          }
+        }
+      });
+    });
+  }
+
+  const additionsGroup = document.querySelector('.addition');
+  if (additionsGroup) {
+    const buttons = additionsGroup.querySelectorAll('button');
+
+    const selectedAdditions = new Set();
+
+    buttons.forEach((button) => {
+      button.addEventListener('click', () => {
+        const addition = button.getAttribute('data-addition');
+        console.log('addition clicked');
+
+        if (selectedAdditions.has(addition)) {
+          selectedAdditions.delete(addition);
+          button.classList.remove('selected');
+        } else {
+          selectedAdditions.add(addition);
           button.classList.add('selected');
         }
-      }
-
+      });
     });
-  });
-}
-
-const additionGroup = document.querySelector('.addition'); 
-if (additionGroup) {
-  const buttons = additionGroup.querySelectorAll('button');
-
-  buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-    });
-  });
-}
+  }
+});
