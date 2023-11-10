22c25c8e26af6c33d7e4f2bc27369f570457943f
Vincent Kosteyev Bechmann <vbechm22@student.aau.dk>
2023-11-10 23:37:02 +0100

    Added a constructor to DaySelector

--- src/main/java/dk/aau/student/AktuelMenu/DaySelector.java
+++ src/main/java/dk/aau/student/AktuelMenu/DaySelector.java
@@ -33,4 +33,16 @@ public class DaySelector {
         sevenDays.sunday = true;
         return sevenDays;
     }
+
+    // This could be done so much easier with bit flags, but I know for a fact that if I write them,
+    // they'll show up to the exam and you guys will have no idea how to use them
+    public DaySelector(boolean monday, boolean tuesday, boolean wednesday, boolean thursday, boolean friday, boolean saturday, boolean sunday){
+        this.monday = monday;
+        this.tuesday = tuesday;
+        this.wednesday = wednesday;
+        this.thursday = thursday;
+        this.friday = friday;
+        this.saturday = saturday;
+        this.sunday = sunday;
+    }
 }
