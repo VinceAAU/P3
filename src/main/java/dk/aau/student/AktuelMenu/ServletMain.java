0024d83fa843c8bcbae7c503637b4dee7f1e338f
nicol <nicolai.nicolai.berwald@gmail.com>
2023-11-10 09:38:01 +0100

    boilerplating, alot of lines no real meat

--- src/main/java/dk/aau/student/AktuelMenu/ServletMain.java
+++ src/main/java/dk/aau/student/AktuelMenu/ServletMain.java
@@ -23,6 +23,23 @@ public class ServletMain extends HttpServlet {
         burger.addOption(new Option("Vegi","Vegi",0));
         menu.addItem(burger);
 
+        MenuItem variant = new MenuItem("xVarianter","Vælg 3 varianter eller flere",0);
+        variant.addOption(new Option("Andelår","Confiteret andelår med skysauce",105));
+        variant.addOption(new Option("Bao m grisebryst","Bao med mørt grisebryst marineret i Hoisin BBQ, spidskål, ristet sesam mayo og forårsløg",80));
+        variant.addOption(new Option("Miso Rejer","Miso ramen med udon nudler, æg, forårsløg, chili og panko rejer",80));
+        variant.addOption(new Option("Røget Laks","Skiver af kold røget laks med sesammayo, wasabi og ærteskud",80));
+        variant.addOption(new Option("Tarteletter","Tarteletter med høns i asparges creme, tomat og persille",80));
+        variant.addOption(new Option("Vinterslider m Ribbensteg","Vinterslider med ribbensteg, aioli, hjemmelavet rødkål og flæskesværs knas",80));
+        variant.addOption(new Option("Svinekæber","Svinekæber ala creme med stegte svampe og svampesauce",80));
+        variant.addOption(new Option("Pankorejer","Dybstegte pankorejer med chili mayo",80));
+        variant.addOption(new Option("Fried Chicken","Fried chicken med chili mayo",80));
+        variant.addOption(new Option("Tatar","Rørt tatar med marineret kål, grøn mayo, syltede rødløg, jordskokkechips og karse med rå æggeblomme",80));
+        variant.addOption(new Option("lakse tatar","Rørt laksetatar med urter, avocadocreme og sprøde rugchips",80));
+        variant.addOption(new Option("veggie Tatar","Veggie tatar på gulerod og sesam, med marineret kål, grøn mayo, edamamebønner, jordskokkechips og karse",80));
+
+        MenuItem SliderFafal = new MenuItem("SliderFafel","Slider med falafel, feldsalat og chimichurri af soltørret tomat og peberfrugt",80);
+
+
         ArrayList<Option> optionsInOrder = new ArrayList<>(List.of(new Option[]{burger.getOptions()[0]}));
         OrderItem order1 = new OrderItem(burger,optionsInOrder, new ArrayList<>());
 
