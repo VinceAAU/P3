3d4d09d871cf7cee0bc9b19f9db738983ad96bca
ZeroConfidence <amoldt22@student.aau.dk>
2023-11-11 16:26:51 +0100

    reworked the order system with check boxes frontend order generation now works

--- src/main/webapp/WEB-INF/Dyna_menu_js.js
+++ src/main/webapp/WEB-INF/Dyna_menu_js.js
@@ -6,103 +6,72 @@ class Order_Item {
   }
 }
 
-const order = [];
+document.addEventListener("DOMContentLoaded", function () {
+  let optionGroups = document.querySelectorAll('.option');
 
-function addToOrder(itemName, selectedOptions, selectedAdditions) {
-  const orderItem = new Order_Item(itemName);
-  orderItem.selectedOptions = selectedOptions;
-  orderItem.selectedAdditions = selectedAdditions;
-  order.push(orderItem);
+  optionGroups.forEach(function (optionGroup) {
+    let minSelections = optionGroup.getAttribute('data-min-selections');
+    let maxSelections = optionGroup.getAttribute('data-max-selections');
 
-  console.log('item added to order', orderItem);
-}
+    let checkboxes = optionGroup.querySelectorAll('input[type="checkbox"]');
 
-document.querySelector('div > button.add-to-order').addEventListener('click', (event) => {
-  const target = event.target;
+    checkboxes.forEach(function (checkbox) {
+      checkbox.addEventListener('change', function () {
+        let checkedCount = optionGroup.querySelectorAll('input[type="checkbox"]:checked').length;
 
-  if (target.classList.contains('add-to-order')) {
-    const itemName = target.getAttribute('data-item-name');
-    const optionsGroup = document.querySelector('.option');
-    const selectedOptions = getSelectedOptions(optionsGroup);
-    const additionGroup = document.querySelector('.addition');
-    const selectedAdditions = getSelectedAdditions(additionGroup);
-
-    addToOrder(itemName, selectedOptions, selectedAdditions);
-  }
-
-  function getSelectedOptions(group) {
-    const buttons = group.querySelectorAll('button');
-    const selectedOptions = [];
-
-    buttons.forEach((button) => {
-      if (button.classList.contains('selected')) {
-        const option = button.getAttribute('data-option');
-        selectedOptions.push(option);
-      }
-    });
-
-    return selectedOptions;
-  }
-
-  function getSelectedAdditions(group) {
-    const buttons = group.querySelectorAll('button');
-    const selectedAdditions = [];
-
-    buttons.forEach((button) => {
-      if (button.classList.contains('selected')) {
-        const addition = button.getAttribute('data-addition');
-        selectedAdditions.push(addition);
-      }
-    });
-
-    return selectedAdditions;
-  }
-
-  const optionsGroup = document.querySelector('.option');
-  if (optionsGroup) {
-    const buttons = optionsGroup.querySelectorAll('button');
-    const minSelections = parseInt(optionsGroup.getAttribute('data-min-selections'));
-    const maxSelections = parseInt(optionsGroup.getAttribute('data-max-selections'));
-
-    const selectedOptions = new Set();
-
-    buttons.forEach((button) => {
-      button.addEventListener('click', () => {
-        const option = button.getAttribute('data-option');
-        console.log('option clicked');
-
-        if (selectedOptions.has(option)) {
-          selectedOptions.delete(option);
-          button.classList.remove('selected');
-        } else {
-          if (selectedOptions.size < maxSelections) {
-            selectedOptions.add(option);
-            button.classList.add('selected');
-          }
+        if (checkedCount > maxSelections) {
+          alert('Maximum selections exceeded.');
+          checkbox.checked = false;
         }
       });
     });
-  }
+  });
+});
 
-  const additionsGroup = document.querySelector('.addition');
-  if (additionsGroup) {
-    const buttons = additionsGroup.querySelectorAll('button');
+document.addEventListener("DOMContentLoaded", function () {
+  let addToOrderButtons = document.querySelectorAll('.add-to-order');
 
-    const selectedAdditions = new Set();
+  addToOrderButtons.forEach(function (addToOrderButton) {
+    addToOrderButton.addEventListener('click', function () {
+      // Find the closest item container
+      let itemContainer = addToOrderButton.closest('.item-container');
 
-    buttons.forEach((button) => {
-      button.addEventListener('click', () => {
-        const addition = button.getAttribute('data-addition');
-        console.log('addition clicked');
+      if (!itemContainer) {
+        console.error('Could not find the closest .item-container element.');
+        return;
+      }
 
-        if (selectedAdditions.has(addition)) {
-          selectedAdditions.delete(addition);
-          button.classList.remove('selected');
-        } else {
-          selectedAdditions.add(addition);
-          button.classList.add('selected');
-        }
+      // Find the option group within the item container
+      let optionGroup = itemContainer.querySelector('.option');
+
+      if (!optionGroup) {
+        console.error('Could not find the .option element within the item container.');
+        return;
+      }
+
+      let itemName = itemContainer.querySelector('h3').textContent.trim();
+
+      let selectedOptions = [];
+      optionGroup.querySelectorAll('input[type="checkbox"]:checked').forEach(function (checkbox) {
+        selectedOptions.push(checkbox.getAttribute('data-option'));
       });
+
+      let selectedAdditions = [];
+      itemContainer.querySelectorAll('.addition input[type="checkbox"]:checked').forEach(function (checkbox) {
+        selectedAdditions.push(checkbox.getAttribute('data-addition'));
+      });
+
+      let quantity = parseInt(itemContainer.querySelector('#item-quantity').value, 10);
+
+      let orderItems = [];
+      for (let i = 0; i < quantity; i++) {
+        let orderItem = new Order_Item(itemName);
+        orderItem.selectedOptions = selectedOptions.slice();
+        orderItem.selectedAdditions = selectedAdditions.slice();
+        orderItems.push(orderItem);
+      }
+
+      console.log(orderItems);
     });
-  }
+  });
 });
