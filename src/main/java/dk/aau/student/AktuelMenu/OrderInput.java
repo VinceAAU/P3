2fbfda2fbc742bf872a23e322463d4b7d4ebbd0d
ZeroConfidence <113094244+ZeroConfidence@users.noreply.github.com>
2023-11-17 11:07:46 +0100

    working on api stuff uwu

---
+++ src/main/java/dk/aau/student/AktuelMenu/OrderInput.java
@@ -0,0 +1,9 @@
+package dk.aau.student.AktuelMenu;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "orderinput", value = "/OrderSent")
+public class OrderInput extends HttpServlet {
+
+}
