9606ab4e59fe7ac68664d93f9e78be144fb68951
nicol <nicolai.nicolai.berwald@gmail.com>
2023-11-17 13:01:31 +0100

    json sender Co-authored-by: senan2315 <40441468+senan2315@users.noreply.github.com>

--- src/main/webapp/Adminjs.js
+++ src/main/webapp/Adminjs.js
@@ -199,7 +199,27 @@ function SendMenuToServer()
     }
 
 }
+    const sendUrl = 'http://localhost:8080//AktuelMenu/MenuSent'
 
+    fetch(sendUrl,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(json)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 
 }
 
